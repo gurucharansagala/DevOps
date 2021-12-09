8eac2611618d7d5ccdb08c1dbd5776d5bc434707
Gurucharan <thejacharan0807@gmail.com>
2021-12-10 02:06:19 +0530

    added colors for echo

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo Hello World
+## we can print text in colors
+# syntax : echo -e "\e[COLmMessage\e[0m"
+## -e :to enable \e
+## \e[ :to enable colors
+## COL :color code
+## m : end of syntax
+## 0 :to disable color
+
+## color codes: red 31, green 32, yellow 33, blue 34, magenta 35, cyan 36
+
+echo -e "\e[31mHello In Red Color\e[0m"
+echo -e "\e[32mHello In Green Color\e[0m"
+echo -e "\e[33mHello In Yellow Color\e[0m"
+echo -e "\e[34mHello In Blue Color\e[0m"
+echo -e "\e[35mHello In Magenta Color\e[0m"
+echo -e "\e[36mHello In Cyan Color\e[0m"
