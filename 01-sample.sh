8e770a2ef9aea84ed9d05a65c9873d232aaa1540
Gurucharan <thejacharan0807@gmail.com>
2021-12-20 19:59:37 +0530

    Refractor the files into shell-scripting folder

--- 01-sample.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This is the sample script
-ls -ltr
---
+++ shell-scripting/01-sample.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is the sample script
+ls -ltr
+
+
+## To execute the script we can execute with
+# bash script or sh script
+
+#In Redhat family bash and sh are same. in other os like ubuntu those are different.
